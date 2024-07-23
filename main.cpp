e3b9fb46f9a650871812ec28bc01df7f19a66d0f
Raghad Ali <143431529+raghad-smth@users.noreply.github.com>
2024-07-23 18:56:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
+//FCA  OOP Programming – 2023 - Assignment 1
+//Program Name: main.cpp
+//Last Modification Date: 9.10.2023
+//Purpose:  Different functions that process 256x256 bitmap images read into a 2D array
+/*
+ * Name  : Raghad Mohammed Suliman Mohammed
+ * ID    : 20220704
+ * Email : raghaduni1004@gmail.com
+ *
+
+ * Name  : Aseel Mutasim Mohammed
+ * ID    : 20220754
+ * Email : aseelmutasim9204@gmail.com
+ *
+ * Name  :  Ruba Awadallah Mohammed
+ * ID    :  20220843
+ * Email :  rubaawadalla88@gmail.com
+*/
+
 #include "menu.h"
 
 int main() {
